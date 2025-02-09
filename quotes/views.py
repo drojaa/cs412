bb87fc98458b5409890f8743da0f359ad8daf8cb
Derinell Rojas <119087348+drojaa@users.noreply.github.com>
2025-02-08 20:06:05 -0500

    Update views.py

--- quotes/views.py
+++ quotes/views.py
@@ -6,8 +6,13 @@ from django.http import HttpRequest, HttpResponse
 import random
 import time
 
+#list containing all quotes
 quotes = ["I don't belong to anyone else but myself. I have to make my own decisions. Happiness is defined by me.", "We don’t all need to look the same. Beauty is in the eye of the beholder, you decide what’s beautiful in the world. Not the other way around.", "Dream big, work hard, and surround yourself with good people", 'Never let your dream die in the midst of doing the things you have to do to get what you need to invest in yourself. This is the hardest part, but you are worth it', 'You have to understand that when things go wrong in your life, it doesn’t mean you need to quit. It means you need to get stronger and change your plan.']
+
+#list containing all images
 images = ["keke1.png", "keke2.png", "keke3.jpg", "keke4.jpg.webp", "keke5.jpg.webp"]
+
+#list containing tuples with both quotes and images
 quotes_map_images = [
     ("I don't belong to anyone else but myself. I have to make my own decisions. Happiness is defined by me.", 'keke1.png'),
     ('We don’t all need to look the same. Beauty is in the eye of the beholder, you decide what’s beautiful in the world. Not the other way around.', 'keke2.png'),
@@ -16,6 +21,7 @@ quotes_map_images = [
     ('You have to understand that when things go wrong in your life, it doesn’t mean you need to quit. It means you need to get stronger and change your plan.', 'keke5.jpg.webp')
 ]
 
+#function for homepage and single quotes
 def quote(request):
   
     template_name = "quotes/quote.html"
@@ -25,7 +31,8 @@ def quote(request):
         "time": time.ctime(), 
     }
     return render(request, template_name, context)
-
+    
+#function for page showing all quotes and all images
 def show_all(request):
 
     template_name = "quotes/show_all.html"
@@ -36,7 +43,7 @@ def show_all(request):
         "time": time.ctime(), 
     }
     return render(request, template_name, context)
-
+#function for about page
 def about(request):
 
     template_name = "quotes/about.html"
