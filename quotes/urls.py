ac847fc6300541297e146a221cc2b7dc0310c0ef
Derinell Rojas <119087348+drojaa@users.noreply.github.com>
2025-02-08 19:53:11 -0500

    Update urls.py

--- quotes/urls.py
+++ quotes/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from django.conf import settings
 from . import views 
-
+# URL pattern list defining the routing of requests to specific views
 urlpatterns = [
     path(r'', views.quote, name="quote"),
     path(r'quote', views.quote, name="quote"), 
