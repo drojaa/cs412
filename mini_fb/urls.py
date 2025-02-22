71fa52de19fbdd24a0c3793076887ada383bd745
drojaa <droja@bu.edu>
2025-02-21 21:15:01 -0500

    Created Table with Data

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -3,8 +3,8 @@
 
 from django.urls import path
 from django.conf import settings
-from . import views 
+from .views import ShowAllProfilesView
 
 urlpatterns = [
-   # path(r'', views.home, name="home"), 
+   path('', ShowAllProfilesView.as_view(), name="show_all_profiles"), 
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Profile
 # Create your views here.
+import time
+# function is used to display all profiles at once
+class ShowAllProfilesView(ListView):
+    '''Define a view class to show all Profiles'''
+
+    model = Profile
+    template_name = "mini_fb/show_all_profiles.html"
+    context_object_name = "profiles"
+   
