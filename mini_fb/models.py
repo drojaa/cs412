0a497423d60a9a7ee05249b18b789c3edbaa9cd2
drojaa <droja@bu.edu>
2025-02-27 12:34:23 -0500

    Created Status Model and Displayed all

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 from django.db import models
+import time
 # Define data models for mini_fb
 # Create your models here.
 
@@ -19,3 +20,20 @@ class Profile(models.Model):
     def __str__(self):
         '''Return a string representation of this Profile object.'''
         return f'{self.first_name} {self.last_name}'
+    def get_status_messages(self):
+        '''returns all messages for specifc Profile'''
+        status_messages = StatusMessage.objects.filter(profile=self).order_by('timestamp')
+        return status_messages
+
+
+class StatusMessage(models.Model): 
+    '''Encapsulate the data of Facebook Status Message for each Profile'''
+
+    # define the data attributes of the Profile object 
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    timestamp =  models.DateTimeField(auto_now=True)
+    message = models.TextField(blank=False)
+    
+    def __str__(self):
+        '''Returns a string representation of this Status Message'''
+        return f'{self.message}'
