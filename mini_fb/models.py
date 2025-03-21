9d580821b520cd281945e3b6acb4e64f66ad7037
drojaa <droja@bu.edu>
2025-03-21 15:02:22 -0400

    get_friends method + css styling

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -35,7 +35,26 @@ class Profile(models.Model):
     def get_absolute_url(self):
         '''Return a URL to display once instance of this object'''
         return reverse('show_profile', kwargs={'pk':self.pk})
-
+    def get_friends(self):
+        '''Return a List of given profile's friends'''
+        friend_list = []
+        friends = Friend.objects.filter(profile1=self) | Friend.objects.filter(profile2=self)
+        '''Checks if the friend isn't already inside the list'''
+        '''Checks if profile1 or profile2 isn't the self object'''
+        for friend in friends:
+            if friend.profile1 not in friend_list:
+                if self.first_name != friend.profile1.first_name:
+                    if self.last_name != friend.profile1.last_name:
+                         prof_friend = Profile.objects.get(first_name=friend.profile1.first_name, last_name=friend.profile1.last_name)
+                         friend_list.append(prof_friend)
+            if friend.profile2 not in friend_list:
+                if self.first_name != friend.profile2.first_name:
+                    if self.last_name != friend.profile2.last_name:
+                        prof_friend = Profile.objects.get(first_name=friend.profile2.first_name, last_name=friend.profile2.last_name) 
+                        print(prof_friend)
+                        friend_list.append(prof_friend)
+        return friend_list
+       
 class StatusMessage(models.Model): 
     '''Encapsulate the data of Facebook Status Message for each Profile'''
 
@@ -64,3 +83,12 @@ class StatusImage(models.Model):
     '''Encap Image for Status Message'''
     img_file = models.ForeignKey(Image, on_delete=models.CASCADE)
     stat_msg = models.ForeignKey(StatusMessage, on_delete=models.CASCADE)
+
+class Friend(models.Model):
+    '''Encaps the Friend relationship amongst Profiles'''
+    profile1 =  models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="profile1")
+    profile2 =  models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="profile2")
+    timestamp =  models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.profile1} and  {self.profile2}'
