e8fbe0be456a5eaa302a35e5267c6522bec472d4
drojaa <droja@bu.edu>
2025-10-08 08:20:53 -0400

    ready func

---
+++ voter_analytics/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'voter_analytics.apps.VoterAnalyticsConfig'
--- voter_analytics/apps.py
+++ voter_analytics/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class VoterAnalyticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'voter_analytics'
+
+    def ready(self):
+        import os
+        from django.conf import settings
+        from .models import Voter
+
+        # Prevent running multiple times in dev server autoreload
+        if os.environ.get('RUN_MAIN') == 'true':
+            Voter.load_data()
